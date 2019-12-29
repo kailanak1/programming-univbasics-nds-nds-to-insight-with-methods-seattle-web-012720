283b18dd1de2df231277c20c9354adc3f387da73
Kailana Kahawaii <kailanakahawaii@gmail.com>
2019-12-29 20:41:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -23,6 +23,7 @@ def directors_totals(nds)
    directors = nds[i][:name]
    total = gross_for_director(nds[i])
    i += 1 
+   print total 
   end 
-  result[directors] = total.to_i 
+  result[directors] = total
 end
